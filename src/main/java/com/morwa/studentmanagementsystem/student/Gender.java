2191330c8fb176004bfdd9e0a2aa1279d5b6bdc5
Kamogelo <kwmmatli@gmail.com>
2022-03-28 22:19:18 +0200

    init student-management-system

---
+++ src/main/java/com/morwa/studentmanagementsystem/student/Gender.java
@@ -0,0 +1,7 @@
+package com.morwa.studentmanagementsystem.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/morwa/studentmanagementsystem/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.morwa.studentmanagementsystem.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        var students = Arrays.asList(
+                new Student(1L, "kamo", "kwmmatli@example.com", Gender.MALE),
+                new Student(2L, "Mathapelo", "mat@example.com", Gender.FEMALE)
+        );
+
+        return students;
+    }
+}
